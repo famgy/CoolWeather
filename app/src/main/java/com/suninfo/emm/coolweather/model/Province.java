68095becd62d69008ca10d8c98fd3f1c683e3cf2
famgy <famgy@debian>
2017-09-01 15:29:27 +0800

    add weather db

---
+++ app/src/main/java/com/suninfo/emm/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.suninfo.emm.coolweather.model;
+
+/**
+ * Created by famgy on 9/1/17.
+ */
+
+public class Province {
+    private  int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
